1cf293a477aa230a6f4ef72075123497d21cfad4
MilanManoj <166032238+MilanM002005@users.noreply.github.com>
2026-08-04 22:15:01 +0530

    Time: 7 ms (96.48%), Space: 19.8 MB (9.55%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,25 +1,32 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def solveNQueens(self, n: int):
         res = []
         board = [["."] * n for _ in range(n)]
-        col = [False] * n
-        diag1 = [False] * (2 * n - 1)  
-        diag2 = [False] * (2 * n - 1)  
 
-        def backtrack(row: int):
+        cols = set()
+        diag1 = set()  # row - col
+        diag2 = set()  # row + col
+
+        def backtrack(row):
             if row == n:
                 res.append(["".join(r) for r in board])
                 return
 
-            for c in range(n):
-                d1 = row - c + n - 1
-                d2 = row + c
-                if not col[c] and not diag1[d1] and not diag2[d2]:
-                    col[c] = diag1[d1] = diag2[d2] = True
-                    board[row][c] = "Q"
-                    backtrack(row + 1)
-                    board[row][c] = "."
-                    col[c] = diag1[d1] = diag2[d2] = False
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+
+                board[row][col] = "Q"
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+
+                board[row][col] = "."
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
 
         backtrack(0)
         return res
