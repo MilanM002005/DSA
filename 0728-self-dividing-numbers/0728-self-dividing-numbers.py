5f0f70fb0c5e372a4c95e30385d6496429fb8afc
MilanManoj <166032238+MilanM002005@users.noreply.github.com>
2026-08-09 20:31:13 +0530

    Time: 3 ms (96.32%), Space: 19.2 MB (74.1%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        ans = []
+        for n in range(left, right + 1):
+            x = n
+            while x:
+                d = x % 10
+
+                if d == 0 or n % d:
+                    break
+
+                x //= 10
+            else:
+                ans.append(n)
+        return ans
